2a03811647c442d163a4259690636afd737a63fa
Akankshavijay <akanksha.vijayvergiya001@stud.fh-dortmund.de>
2025-12-07 00:58:03 +0100

    Implement OrderRepository and move order code into feature branch

--- Backend/4EverBloom/src/main/java/de/fhdo/project/blumeo/repository/order/OrderRepository.java
+++ Backend/4EverBloom/src/main/java/de/fhdo/project/blumeo/repository/order/OrderRepository.java
@@ -1,5 +1,9 @@
 package de.fhdo.project.blumeo.repository.order;
 
-public class OrderRepository {
+import de.fhdo.project.blumeo.entity.order.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
