fb9bb6f9ac11ed2b905e962a051764061598e505
“frauAnastasiia” <kulyani2003@outlook.com>
2025-12-17 11:54:25 +0100

    moved config file to another directory

---
+++ ClickPrototype/config/api.config.js
@@ -0,0 +1,6 @@
+export const API_MODE = "REST"; //or "GRAPHQL"
+
+export const API_BASE_URL = "http://localhost:8080";
+
+export const REST_BASE = `${API_BASE_URL}/api/v1`;
+export const GRAPHQL_ENDPOINT = `${API_BASE_URL}/graphql`;
