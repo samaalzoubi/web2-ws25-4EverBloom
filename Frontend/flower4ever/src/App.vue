247722d1c1ce8c17a3828947076f21a59fc91097
avosm001 <averest.osman001@stud.fh-dortmund.de>
2025-12-17 22:30:53 +0100

    Implemented the FRONTEND with vue.js

---
+++ Frontend/flower4ever/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Header />
+    <router-view />
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from './components/Footer/Footer.vue';
+import Header from './components/Header/Header.vue';
+
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+
+<style>
+
+</style>
